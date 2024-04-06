ff31241e169cd19c2765a4cf3a15af5106b94d98
Vishnudas Mankar <vmankar@avaya.com>
2024-04-06 22:59:07 +0530

    second commit

--- src/main/java/DemoJavaApplication.java
+++ src/main/java/DemoJavaApplication.java
@@ -2,5 +2,6 @@ public class DemoJavaApplication {
 
     public static void main(String[] args) {
         System.out.println("hello Good morning ");
+        System.out.println("hello how are you ? ");
     }
 }
